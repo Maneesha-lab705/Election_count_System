f9dfe6f05649694a3e4a62d4b473cc2d7bee005f
Maneesha-lab705 <124575388+Maneesha-lab705@users.noreply.github.com>
2024-10-02 15:00:18 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component'
+import {ProfileComponent} from './profile/profile.component';
+import {NgModule} from "@angular/core";
+
+export const routes: Routes = [
+    {path :'',component: HomeComponent},
+    {path :'profile',component: ProfileComponent}
+];
+
+@NgModule({
+  imports:[RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+})
+
+export class AppRoutes{}
+
+
